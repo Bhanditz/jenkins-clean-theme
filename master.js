71169d85317de2031c522a218dc1971792afbca7
Amos Wenger <fasterthanlime@gmail.com>
2015-12-06 23:16:54 +0100

    favico, some padding

---
+++ master.js
@@ -0,0 +1,7 @@
+(function() {
+  var link = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = 'https://rawgit.com/fasterthanlime/jenkins-clean-theme/master/favicon.ico';
+  document.getElementsByTagName('head')[0].appendChild(link);
+}());
